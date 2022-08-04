c0c631b14267381ea551bc8b887abb7043c3135c
Artyom Pankov <apankov4696@gmail.com>
2022-08-04 19:53:00 +0300

    feat: special case solution underway

--- src/index.js
+++ src/index.js
@@ -1,20 +1,37 @@
-module.exports = function check(str, bracketsConfig) {
+// module.exports = function check(str, bracketsConfig) {
+function check(str, bracketsConfig) {
   let stack = [];
   let bracketsPairs = {};
   for (let brackets of bracketsConfig) {
     bracketsPairs[brackets[1]] = brackets[0];
   }
+  console.log(bracketsPairs);
   for (let i = 0; i < str.length; i++) {
-    if (Object.keys(bracketsPairs).includes(str[i]) || Object.values(bracketsPairs).includes(str[i])) {
-      if (Object.values(bracketsPairs).includes(str[i])) {
-        stack.push(str[i]);
-      }
-      if (Object.keys(bracketsPairs).includes(str[i])) {
-        if (bracketsPairs[str[i]] != stack.pop(str[i])) {
-          return false;
-        }
-      }
+
+    if (Object.values(bracketsPairs).includes(str[i])) {
+      stack.push(str[i]);
+    } else {
+      if (stack.length === 0) return false;
+    
+      let topBracket = stack[stack.length - 1];
+
+      if (bracketsPairs[str[i]] == topBracket) {
+        stack.pop(str[i]);
+      } else return false
     }
   }
-  return stack.length === 0;
+  return stack;
 }
+
+// console.log(check('()', [['(', ')']])); // -> true
+// console.log(check('((()))()', [['(', ')']])); // -> true
+// console.log(check('())(', [['(', ')']])); // -> false
+// console.log(check('([{}])', [['(', ')'], ['[', ']'], ['{', '}']])); // -> true
+// console.log(check('[(])', [['(', ')'], ['[', ']']])); // -> false
+// console.log(check('[]()', [['(', ')'], ['[', ']']])); // -> true
+// console.log(check('[]()(', [['(', ')'], ['[', ']']])); // -> false
+
+// console.log(check('||', [['|', '|']])); // -> true
+console.log(check('|()|', [['(', ')'], ['|', '|']])); // -> true
+// console.log(check('|(|)', [['(', ')'], ['|', '|']])); // -> false
+// console.log(check('|()|(||)||', [['(', ')'], ['|', '|']])); // -> true
