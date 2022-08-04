5e5006b131495a0f1e72ec9064bab03c9bcaa872
Панков Артем Владимирович <pankov_AV@GOIK.LOCAL>
2022-08-04 16:14:00 +0300

    feat: add function without special case

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let bracketsPairs = {};
+  for (let brackets of bracketsConfig) {
+    bracketsPairs[brackets[1]] = brackets[0];
+  }
+  for (let i = 0; i < str.length; i++) {
+    if (Object.keys(bracketsPairs).includes(str[i]) || Object.values(bracketsPairs).includes(str[i])) {
+      if (Object.values(bracketsPairs).includes(str[i])) {
+        stack.push(str[i]);
+      }
+      if (Object.keys(bracketsPairs).includes(str[i])) {
+        if (bracketsPairs[str[i]] != stack.pop(str[i])) {
+          return false;
+        }
+      }
+    }
+  }
+  return stack.length === 0;
 }
