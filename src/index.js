f5bbf2ee16d4c22dd7056a34011153c9ed68b563
Панков Артем Владимирович <pankov_AV@GOIK.LOCAL>
2022-08-05 09:43:51 +0300

    feat: special case underway

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-// module.exports = function check(str, bracketsConfig) {
-function check(str, bracketsConfig) {
+module.exports = function check(str, bracketsConfig) {
+// function check(str, bracketsConfig) {
   let stack = [];
   let bracketsPairs = {};
   for (let brackets of bracketsConfig) {
     bracketsPairs[brackets[1]] = brackets[0];
-  }
-  console.log(bracketsPairs);
+  } 
+  
   for (let i = 0; i < str.length; i++) {
-
-    if (Object.values(bracketsPairs).includes(str[i])) {
-      stack.push(str[i]);
-    } else {
-      if (stack.length === 0) return false;
-    
-      let topBracket = stack[stack.length - 1];
-
-      if (bracketsPairs[str[i]] == topBracket) {
-        stack.pop(str[i]);
-      } else return false
+    if (Object.keys(bracketsPairs).includes(str[i]) || Object.values(bracketsPairs).includes(str[i])) {
+      let current = str[i];
+      if (Object.values(bracketsPairs).includes(current)) {
+        stack.push(current);
+      } else {
+        if (stack.length === 0) return false;
+        let topBracket = stack[stack.length - 1];
+        if (bracketsPairs[current] === topBracket) {
+          stack.pop();
+        }
+      }
     }
   }
-  return stack;
+  return stack.length === 0
 }
 
 // console.log(check('()', [['(', ')']])); // -> true
@@ -32,6 +32,6 @@ function check(str, bracketsConfig) {
 // console.log(check('[]()(', [['(', ')'], ['[', ']']])); // -> false
 
 // console.log(check('||', [['|', '|']])); // -> true
-console.log(check('|()|', [['(', ')'], ['|', '|']])); // -> true
+// console.log(check('|()|', [['(', ')'], ['|', '|']])); // -> true
 // console.log(check('|(|)', [['(', ')'], ['|', '|']])); // -> false
 // console.log(check('|()|(||)||', [['(', ')'], ['|', '|']])); // -> true
