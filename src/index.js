91704d03cb0e0fa619b9803eefb5db736f38dfc5
Artyom Pankov <apankov4696@gmail.com>
2022-08-06 18:22:24 +0300

    feat: add solution with special case

--- src/index.js
+++ src/index.js
@@ -1,26 +1,46 @@
-module.exports = function check(str, bracketsConfig) {
-// function check(str, bracketsConfig) {
-  let stack = [];
-  let bracketsPairs = {};
-  for (let brackets of bracketsConfig) {
-    bracketsPairs[brackets[1]] = brackets[0];
-  } 
+// Using stack, without special case when opening and closing bracket can be the same
+// module.exports = function check(str, bracketsConfig) {
+//   let stack = [];
+//   let bracketsPairs = {};
+//   for (let brackets of bracketsConfig) {
+//     bracketsPairs[brackets[1]] = brackets[0];
+//   } 
   
-  for (let i = 0; i < str.length; i++) {
-    if (Object.keys(bracketsPairs).includes(str[i]) || Object.values(bracketsPairs).includes(str[i])) {
-      let current = str[i];
-      if (Object.values(bracketsPairs).includes(current)) {
-        stack.push(current);
-      } else {
-        if (stack.length === 0) return false;
-        let topBracket = stack[stack.length - 1];
-        if (bracketsPairs[current] === topBracket) {
-          stack.pop();
+//   for (let i = 0; i < str.length; i++) {
+//     if (Object.keys(bracketsPairs).includes(str[i]) || Object.values(bracketsPairs).includes(str[i])) {
+//       let current = str[i];
+//       if (Object.values(bracketsPairs).includes(current)) {
+//         stack.push(current);
+//       } else {
+//         if (stack.length === 0) return false;
+//         let topBracket = stack[stack.length - 1];
+//         if (bracketsPairs[current] === topBracket) {
+//           stack.pop();
+//         }
+//       }
+//     }
+//   }
+//   return stack.length === 0
+// }
+
+module.exports = function check(str, bracketsConfig) {
+    const parsedBracketsConfig = bracketsConfig.map((item) => {
+        return `${item[0]}${item[1]}`;
+    });
+    let prevLength = str.length;
+
+    while (str.length) {
+        parsedBracketsConfig.forEach((item) => {
+          str = str.replaceAll(item, "");
+        });
+        if (str.length === prevLength) {
+            return false;
         }
-      }
+
+        prevLength = str.length;
     }
-  }
-  return stack.length === 0
+
+    return true;
 }
 
 // console.log(check('()', [['(', ')']])); // -> true
